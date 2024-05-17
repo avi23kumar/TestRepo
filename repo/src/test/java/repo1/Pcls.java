3d93e14d5b13688655f540ee9db19ed8d74523c3
Avinashkumar s m <avinashkumar1998dgd@gmail.com>
2024-05-17 11:04:44 +0530

    55

---
+++ repo/src/test/java/repo1/Pcls.java
@@ -0,0 +1,16 @@
+package repo1;
+
+import org.testng.annotations.Test;
+
+public class Pcls {
+	@Test
+	public void maintest()
+	{
+		System.out.println("print kumar");
+	}
+	@Test
+	public void ittest() {
+		System.out.println("print kumarls");
+	}
+
+}
---
+++ repo/src/test/java/repo3/LIO.java
@@ -0,0 +1,17 @@
+package repo3;
+
+import org.testng.annotations.Test;
+
+public class LIO {
+	@Test
+	public void main1test()
+	{
+		System.out.println("print ln");
+	}
+	@Test
+	public void kltest()
+	{
+		System.out.println("print kl");
+	}
+
+}
