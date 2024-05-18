39ecb314c1c876aba30cd92a3bef71d47e44d16e
Avinashkumar s m <avinashkumar1998dgd@gmail.com>
2024-05-18 10:59:03 +0530

    5555555

---
+++ repo/src/test/java/repo1/Rtest.java
@@ -0,0 +1,17 @@
+package repo1;
+
+import org.testng.annotations.Test;
+
+public class Rtest {
+	@Test
+	public void main1test()
+	{
+		System.out.println("print ln");
+	}
+	@Test
+	public void kltest()
+	{
+		System.out.println("print kl");
+	}
+
+}
---
+++ repo/src/test/java/repo3/Nametest.java
@@ -0,0 +1,14 @@
+package repo3;
+
+import org.testng.annotations.Test;
+
+public class Nametest {
+	@Test
+	public void normaltest()
+	{
+		System.out.println("print kumar");
+		String k = System.getProperty("url");
+		System.out.println(k);
+	}
+
+}
